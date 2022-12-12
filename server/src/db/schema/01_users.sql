e5f8cb2c8384576358f54b95b9b46c8005f7383d
Nicholas <nc_chau@hotmail.com>
2022-12-12 13:10:07 -0500

    schema for users, tasks, comments tables

---
+++ server/src/db/schema/01_users.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(15) NOT NULL,
+  last_name VARCHAR(15) NOT NULL,
+  email VARCHAR(255),
+  password VARCHAR(255),
+  profile_pic VARCHAR(255),
+  date_created TIMESTAMP NOT NULL DEFAULT now()
+);
---
+++ server/src/db/schema/03_tasks.sql
@@ -0,0 +1,15 @@
+CREATE TYPE status AS ENUM ('pending', 'in progress', 'complete');
+
+DROP TABLE IF EXISTS tasks CASCADE;
+CREATE TABLE tasks (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  project_id INTEGER NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+  title VARCHAR(15) NOT NULL,
+  description TEXT,
+  priority INTEGER,
+  current_status status,
+  date_created TIMESTAMP NOT NULL DEFAULT now(),
+  date_started TIMESTAMP,
+  date_due TIMESTAMP
+);
