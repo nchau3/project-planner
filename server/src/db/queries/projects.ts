aafcfa5e97c154fea36c9cac8ee115f7846a822a
Nicholas <nc_chau@hotmail.com>
2022-12-12 16:34:52 -0500

    change query names

--- server/src/db/queries/projects.ts
+++ server/src/db/queries/projects.ts
@@ -1,9 +1,8 @@
 import db from '../connection';
-import { Project } from '../../models/project';
 
 const getAllProjects = () => {
   return db.query('SELECT * FROM projects;')
-    .then((data: {rows: Project[]}) => {
+    .then((data: any) => {
       return data.rows;
     })
 }
--- server/src/db/queries/users.ts
+++ server/src/db/queries/users.ts
@@ -7,4 +7,4 @@ const getAllUsers = () => {
     })
 }
 
-module.exports = { getAllUsers };
+export { getAllUsers };
