26ce450d68652293b9463f749c1566e4b31d759a
Nicholas <nc_chau@hotmail.com>
2022-12-19 16:05:40 -0500

    adjust getAllProjects query to include owner name and number of tasks completed/in progress

--- server/src/db/queries/projects.ts
+++ server/src/db/queries/projects.ts
@@ -1,7 +1,18 @@
 import db from '../connection';
 
 const getAllProjects = () => {
-  return db.query('SELECT * FROM projects;')
+  return db.query(`
+    SELECT 
+      projects.*,
+      first_name,
+      last_name,
+      COUNT(tasks.*) as tasks_total,
+      (SELECT COUNT(*) FROM tasks WHERE status != 'pending' AND status != 'in progress') as tasks_completed
+    FROM projects
+    JOIN users ON owner_id = users.id
+    JOIN tasks ON projects.id = tasks.project_id
+    GROUP BY projects.id, first_name, last_name
+    ;`)
     .then((data: any) => {
       return data.rows;
     })
--- server/src/db/schema/03_tasks.sql
+++ server/src/db/schema/03_tasks.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS tasks CASCADE;
 DROP TYPE IF EXISTS status CASCADE;
 
-CREATE TYPE status AS ENUM ('pending', 'in progress', 'complete');
+CREATE TYPE status_type AS ENUM ('pending', 'in progress', 'complete');
 
 CREATE TABLE tasks (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE tasks (
   title VARCHAR(50) NOT NULL,
   description TEXT,
   priority INTEGER,
-  status status NOT NULL DEFAULT 'pending',
+  status status_type NOT NULL DEFAULT 'pending',
   date_created TIMESTAMP NOT NULL DEFAULT now(),
   date_started TIMESTAMP,
   date_due TIMESTAMP
